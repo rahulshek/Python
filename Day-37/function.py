d5335ecd8fcd0539c8e505af882c221fdb5301a4
rahulshek <shekhawat.rd31@gmail.com>
2026-02-25 18:05:30 +0530

    function

--- Day-37/function.py
+++ Day-37/function.py
@@ -26,18 +26,44 @@
 
 #~---------------------------Unpacking------------------------------------#
 
-def unpacking(a,b,c):
-	return a,b,c
+# def unpacking(a,b,c):
+# 	return a,b,c
 
 
-print(unpacking(*[10,20,30]))
+# print(unpacking(*[10,20,30]))
+# #!output
+# # (10, 20, 30)
+
+# print(unpacking(*{'a':10,'b':20,'c':30}))
+# #!output
+# # ('a', 'b', 'c')
+
+# print(unpacking(*'hii'))
+# #!output
+# # ('h', 'i', 'i')
+
+#*--------------------------------------------------------------------------------------------#
+
+
+# def add(a,b):
+# 	print(a+b)
+
+# add(10,20)
+
+#*--------------------------------------------------------------------------------------------#
+
+
+def info(name, mobnumber , mailid, alternate_number=None):
+	print(name)
+	print(mobnumber)
+	print(mailid)
+	print(alternate_number)
+
+info(name="Rahul",mobnumber=1234567890,mailid="rahul@gmail.com")
+
+
 #!output
-# (10, 20, 30)
-
-print(unpacking(*{'a':10,'b':20,'c':30}))
-#!output
-# ('a', 'b', 'c')
-
-print(unpacking(*'hii'))
-#!output
-# ('h', 'i', 'i')
+# Rahul
+# 1234567890
+# rahul@gmail.com
+# None
