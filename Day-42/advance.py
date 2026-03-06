a84cd186f8dce1a61d5d97098e5af9f8229b4ac9
rahulshek <shekhawat.rd31@gmail.com>
2026-03-06 12:24:39 +0530

    .

--- Day-42/advance.py
+++ Day-42/advance.py
@@ -40,3 +40,29 @@
 # print(list(filter(lambda x: type(x)==int,l)))
 
 #*-----------------------------------------------------------------------------------------------
+
+#? Program to extract even numbers between the range 1 to 10
+
+# even=list(filter(lambda x: x%2==0,list(range(1,11))))
+# print(even)
+
+#*-----------------------------------------------------------------------------------------------
+
+#? Extract all the string values from the tuple only if it is starting with uppercase and ending with lowercase
+# t = (10,2.3,'Supritha','home','pythoN','Ugadi')
+# out= lambda x: type(x)==str and x[0].isupper() and x[-1].islower()
+# print(list(filter(out,t)))
+
+#*-----------------------------------------------------------------------------------------------
+
+#? Program to find the square of all the even numbers from the given list
+# l = [1.0,2,3.0,4,5.0,6]
+# sqr=lambda x: x**2 if x%2==0 else x
+# print(list(map(sqr,l)))
+
+#*-----------------------------------------------------------------------------------------------   
+
+#? Program to extract all the collection values present in a list which has even length
+l = [10,2.3,'sakshi',[10,20,30],(7,8),{1,2,3,4}]
+
+print(list(filter(lambda x: len(str(x))%2==0,l)))
