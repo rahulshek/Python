165f3c35bc6aa12b05a25ba603463cf3ede2ef61
rahulshek <shekhawat.rd31@gmail.com>
2026-03-16 16:04:00 +0530

    Comment out vowel extraction function and decorator for future implementation

--- Day-49/decorator.py
+++ Day-49/decorator.py
@@ -1,18 +1,22 @@
 # Create a function to extract only vowels from the string  
 
-def add_func(funcname):
-    def wrapper(*args, **kwargs):
-        print("Hello Function made by babitaji ❤️")
-        result=funcname(*args, **kwargs)
-        return result
-    return wrapper
+# def add_func(funcname):
+#     def wrapper(*args, **kwargs):
+#         print("Hello Function made by babitaji ❤️")
+#         result=funcname(*args, **kwargs)
+#         return result
+#     return wrapper
 
-@add_func
-def extract(n:str,out=[]):
-    for i in n:
-        if i in "aeiouAEIUO":
-            out.append(i)    
-    return out
+# @add_func
+# def extract(n:str,out=[]):
+#     for i in n:
+#         if i in "aeiouAEIUO":
+#             out.append(i)    
+#     return out
 
-print(extract("hello world"))
+# print(extract("hello world"))
+
+
+#*---------------------------------------------------------------------------------------
+#create a decorator which finds the total exection time of a function
 
